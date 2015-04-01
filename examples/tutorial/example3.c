7f8d834c0eff03fc52a50e8667cb52e0e183d411
Luong Nguyen <luongnv89@gmail.com>
2015-04-01 10:46:13 +0200

    Fix cannot access memory of btcount

--- examples/tutorial/example3.c
+++ examples/tutorial/example3.c
@@ -26,36 +26,43 @@ void shandler(int s){
 void tcp_callback(pntoh_tcp_stream_t stream, pntoh_tcp_peer_t orig, pntoh_tcp_peer_t dest, pntoh_tcp_segment_t seg, int reason, int extra){
     pbyte_count_t btcount = (pbyte_count_t)(stream->udata);
     fprintf(stderr, "\n [%s] %s:%d (%s) -->",ntoh_tcp_get_status(stream->status),inet_ntoa(*(struct in_addr*)&orig->addr),ntohs(orig->port),ntoh_tcp_get_status(orig->status));
-    fprintf(stderr, "%s:%d (%s)\n\t", inet_ntoa(*(struct in_addr*)&dest->addr),ntohs(dest->port),ntoh_tcp_get_status(dest->status));
+    fprintf(stderr, "%s:%d (%s)\n\t\n", inet_ntoa(*(struct in_addr*)&dest->addr),ntohs(dest->port),ntoh_tcp_get_status(dest->status));
     switch(reason){
         /* connection sinchronization */
         case NTOH_REASON_SYNC:
-            switch(extra){
-                case NTOH_REASON_MAX_SYN_RETRIES_REACHED:
-                case NTOH_REASON_MAX_SYNACK_RETRIES_REACHED:
-                case NTOH_REASON_HSFAILED:
-                case NTOH_REASON_EXIT:
-                case NTOH_REASON_TIMEDOUT:
-                case NTOH_REASON_CLOSED:
-                    if(extra==NTOH_REASON_CLOSED){
-                        fprintf(stderr, "\n[i] %s/%s - %s | Connection closed by %s (%s)",ntoh_get_reason(reason),ntoh_get_reason(extra),ntoh_tcp_get_status(stream->status),stream->closedby==NTOH_CLOSEDBY_CLIENT?"Client":"Server",inet_ntoa(*(struct in_addr*)&(stream->client.addr)));
-                    }else {
-                        fprintf(stderr, "\n\t + %s/%s - %s",ntoh_get_reason(reason),ntoh_get_reason(extra),ntoh_tcp_get_status(stream->status));
-                    }
-                    fprintf(stderr, "\n\t[i] Total transfered data:");
-                    fprintf(stderr, "\n\t- Client: %lu bytes",btcount->client);
-                    fprintf(stderr, "\n\t- Server: %lu bytes",btcount->server);
-                    free(btcount);
+            if(extra!=NULL){
+                switch(extra){
+                    case NTOH_REASON_MAX_SYN_RETRIES_REACHED:
+                    case NTOH_REASON_MAX_SYNACK_RETRIES_REACHED:
+                    case NTOH_REASON_HSFAILED:
+                    case NTOH_REASON_EXIT:
+                    case NTOH_REASON_TIMEDOUT:
+                    case NTOH_REASON_CLOSED:
+                        if(extra==NTOH_REASON_CLOSED){
+                            fprintf(stderr, "\n[i] %s/%s - %s | Connection closed by %s (%s)",ntoh_get_reason(reason),ntoh_get_reason(extra),ntoh_tcp_get_status(stream->status),stream->closedby==NTOH_CLOSEDBY_CLIENT?"Client":"Server",inet_ntoa(*(struct in_addr*)&(stream->client.addr)));
+                        }else {
+                            fprintf(stderr, "\n\t + %s/%s - %s",ntoh_get_reason(reason),ntoh_get_reason(extra),ntoh_tcp_get_status(stream->status));
+                        }
+                        fprintf(stderr, "\n\t[i] Total transfered data:\n");
+                        fprintf(stderr, "\n\t- Client: %lu bytes",btcount->client);
+                        fprintf(stderr, "\n\t- Server: %lu bytes",btcount->server);
+                        free(btcount);
                     break;
+                }
             }
             break;
         case NTOH_REASON_DATA:
-            fprintf(stderr, "\n\t+ Segment payload len: %i",seg->payload_len);
+            fprintf(stderr, "\n\t+ Segment payload len: %i \n",seg->payload_len);
             if(orig==&(stream->client))
-                btcount-> client+=seg->payload_len;
+            {
+                btcount->client+=seg->payload_len;
+            }
             else
+            {
                 btcount->server+=seg->payload_len;
-            break;  
+            }
+            
+            break;
     }
 }
 
@@ -87,7 +94,7 @@ int main ( int argc , char *argv[] )
     size_t size_ip=0;
     size_t size_tcp=0;
     size_t size_total=0;
-
+    fprintf(stderr, "\n\n**** LIBNTOH EXAMPLE 3 *** \n\n");
     fprintf( stderr, "\n[i] libntoh version: %s\n", ntoh_version() );
     fprintf( stderr, "\n[i] libpcap version: %s\n", pcap_lib_version());
 
@@ -174,38 +181,50 @@ int main ( int argc , char *argv[] )
         fprintf(stderr, "\n[e] Error %d creating the TCP session: %s",error,ntoh_get_errdesc(error));
         shandler(0);
     }
+    printf("\n[i] A TCP session is created successfully: ");
     /* capture starts */
     while ( ( packet = pcap_next( handle, &header ) ) != 0 )
     {
-        // fprintf ( stderr , "\nGot a packet!");
         /** Chec ip header */
         iphdr = (struct ip*)(packet+SIZE_ETHERNET);
         size_ip=iphdr->ip_hl*4;
         if((size_ip)<sizeof(struct ip))
+        {
+            fprintf(stderr, "\n Invalid ip header");
             continue;
+        }
         /* If it isn't a TCP segment */
         if(iphdr->ip_p!=IPPROTO_TCP)
+        {   
+            fprintf(stderr, "\n It is not TCP protocol");
             continue;
+        }
         /*check TCP header */
         tcphdr = (struct tcphdr*)((unsigned char*)iphdr+size_ip);
         if((size_tcp=tcphdr->th_off*4)<sizeof(struct tcphdr))
+        {
+            fprintf(stderr, "\n Invalid TCP header");
             continue;
+        }
         size_total=ntohs(iphdr->ip_len);
         //fill TCP tuple5 fields 
         ntoh_tcp_get_tuple5(iphdr,tcphdr,&tcpt5);
-
         /* look for this TCP stream */
         if(!(tcpstream=ntoh_tcp_find_stream(tcpsession,&tcpt5))){
+            fprintf(stderr, "\n[i] Creating a new stream");
             btcount=(pbyte_count_t) calloc(1,sizeof(byte_count_t));
-            if(!(tcpstream=ntoh_tcp_new_stream(tcpsession,&tcpt5,&tcp_callback,0,&error,1,1)))
+            if(!(tcpstream=ntoh_tcp_new_stream(tcpsession,&tcpt5,&tcp_callback,(void*)btcount,&error,1,1)))
                 fprintf(stderr, "\n[e] Error %d creating new stream: %s",error,ntoh_get_errdesc(error));
             else{
-                fprintf(stderr, "\n[i] New stream added! %s:%d --> ",inet_ntoa(*(struct in_addr*)&tcpt5.source),ntohs(tcpt5.sport));
+                fprintf(stderr, "\n[i] *** New stream added! %s:%d --> ",inet_ntoa(*(struct in_addr*)&tcpt5.source),ntohs(tcpt5.sport));
                 fprintf(stderr, "%s:%d",inet_ntoa(*(struct in_addr*)&tcpt5.destination),ntohs(tcpt5.dport));
             }
+        }else{
+            fprintf(stderr, "\n[i] Continue with stream %s:%d --> ",inet_ntoa(*(struct in_addr*)&tcpt5.source),ntohs(tcpt5.sport));
+            fprintf(stderr, "%s:%d",inet_ntoa(*(struct in_addr*)&tcpt5.destination),ntohs(tcpt5.dport));
         }
-
-        switch((ret=ntoh_tcp_add_segment(tcpsession,tcpstream,iphdr,size_total,0))){
+        ret=ntoh_tcp_add_segment(tcpsession,tcpstream,iphdr,size_total,0);
+        switch(ret){
             case NTOH_OK:
             case NTOH_SYNCHRONIZING:
             break;
@@ -215,9 +234,9 @@ int main ( int argc , char *argv[] )
             break;
         }
     }
-
+    printf("No more packet\n");
     shandler(0);
-    //dummy return
+    // printf("\n WHAT EVER!\n");
     return 0;
 }
 
